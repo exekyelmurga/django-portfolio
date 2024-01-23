e61b0bfc7ec8e3567cfbd0997d37aee289eae829
Ezequeil Murga OPereyra <exekyel.murga.pereyra@gmail.com>
2024-01-23 12:31:52 -0300

    blog app agregation, templates, routing to get all posts and by id, and models created

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.db.models.fields import CharField
+from django.db.models.fields.files import ImageField
+from django.db.models import DateField, TextField
+import datetime
 
-# Create your models here.
+
+class Post(models.Model):
+    title = CharField(max_length=100)
+    description = TextField(default = 'Default description')
+    image = ImageField(upload_to="blog/images/")
+    date = DateField(datetime.date.today)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+
+def render_posts(request):
+    posts = Post.objects.all()
+    return render(request, "posts.html", {"posts": posts})
+
+def post_details(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'post.html', {'post': post})
